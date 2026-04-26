0830ec78492205af648fb6670745f8c547a63caa
contactumeshjaiswal-artist <contact.umesh.jaiswal@gmail.com>
2026-04-26 20:52:28 +0530

    extended classes

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// Handling the Form Data
+app.use(express.json({limit: "16kb"}))
+
+// Handling URL Data
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// Handling Data Temporarily
+app.use(express.static("public"))
+
+// Get and Set Cookie
+app.use(cookieParser())
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is Running @ PORT NO -----> ${process.env.PORT}`);        
+    })
+})
+.catch((err)=>{
+    console.log("Customized Error ## Index.js ## -----> ", err);    
+})
 
 /*
 
